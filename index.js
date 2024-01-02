1907cfbef3692fb565133249b3af22aa6eb3649e
Dennis Hickox <frozzel@me.com>
2024-01-02 12:46:25 -0500

    refresh token added

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const express = require('express'); // import express
 const querystring = require('querystring'); // import querystring
 const axios = require('axios'); // import axios
 const session = require('express-session');// import express-session
+const NodeCache = require('node-cache'); // import node-cache
 
 
 const app = express(); // create express app
+
+const accessTokenCache = new NodeCache();
 
 app.set('view engine', 'pug'); // set view engine to pug
 
@@ -14,11 +17,36 @@ const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
 const REDIRECT_URI = 'http://localhost:3000/oauth-callback';
 const authUrl = `https://app.hubspot.com/oauth/authorize?client_id=76cb862b-cce8-4c22-9d7f-78587f33d676&redirect_uri=http://localhost:3000/oauth-callback&scope=crm.objects.contacts.read%20crm.objects.contacts.write`;
-
-const tokenStore = {};  
+  
+const refreshTokenStore = {};
 
 const isAuthorized = (userId) => {
-    return tokenStore[userId] ? true : false;
+    return refreshTokenStore[userId] ? true : false;
+}
+
+const getToken = async (userId) => {
+   if (accessTokenCache.get(userId)) {
+        console.log(accessTokenCache.get(userId));
+       return accessTokenCache.get(userId);
+   } else {
+        try {
+
+            const refreshTokenProof = {
+                grant_type: 'refresh_token',
+                client_id: CLIENT_ID,
+                client_secret: CLIENT_SECRET,
+                redirect_uri: REDIRECT_URI,
+                refresh_token: refreshTokenStore[userId]
+            }
+            const responseBody = await axios.post('https://api.hubapi.com/oauth/v1/token', querystring.stringify(refreshTokenProof));
+            refreshTokenStore[userId] = responseBody.data.refresh_token;
+            accessTokenCache.set(userId, responseBody.data.access_token,  Math.round(responseBody.data.expires_in * 0.75));
+            console.log('getting refresh token')
+            return responseBody.data.access_token;
+        } catch (error) {
+            console.error(error);
+        }
+   }
 }
 
 app.use(session({
@@ -29,19 +57,20 @@ app.use(session({
 
 app.get('/', async (req, res) => {
     if (isAuthorized(req.sessionID)) {
-        const accessToken = tokenStore[req.sessionID];
+        const accessToken = await getToken(req.sessionID);
         const headers = {
             Authorization: `Bearer ${accessToken}`,
             'Content-Type': 'application/json'
         };
         const contacts = 'https://api.hubapi.com/crm/v3/objects/contacts';
         try {
-            const resp  = await axios.get(contacts, { headers });
+            const resp = await axios.get(contacts, { headers });
             const data = resp.data;
-            res.render('home', { 
+            res.render('home', {
                 token: accessToken,
                 contacts: data.results
-             })
+            });
+            
         } catch (error) {
             console.error(error);
         }
@@ -62,7 +91,8 @@ app.get('/oauth-callback', async (req, res) => {
     try {
         const responseBody = await axios.post('https://api.hubapi.com/oauth/v1/token', querystring.stringify(authCodeProof));
         // res.json(responseBody.data);
-        tokenStore[req.sessionID] = responseBody.data.access_token;
+        refreshTokenStore[req.sessionID] = responseBody.data.access_token;
+        accessTokenCache.set(req.sessionID, responseBody.data.access_token,  Math.round(responseBody.data.expires_in * 0.75));
         res.redirect('/');
     } catch (error) {
         console.error(error);
